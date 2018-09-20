483b8b41052a3efba1ab2d49c56700253611f916
Ubuntu <ubuntu@ip-172-31-25-191.us-west-2.compute.internal>
2018-09-20 16:24:19 +0000

    Grab data script

---
+++ scripts/get_data.sh
@@ -0,0 +1,16 @@
+mkdir data
+cd data
+wget http://snap.stanford.edu/decagon/bio-decagon-ppi.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-targets.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-targets-all.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-combo.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-mono.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-effectcategories.tar.gz
+
+ tar -xzf bio-decagon-ppi.tar.gz
+ tar -xzf bio-decagon-targets.tar.gz
+ tar -xzf bio-decagon-targets-all.tar.gz
+ tar -xzf bio-decagon-combo.tar.gz
+ tar -xzf bio-decagon-mono.tar.gz
+ tar -xzf bio-decagon-effectcategories.tar.gz
+
